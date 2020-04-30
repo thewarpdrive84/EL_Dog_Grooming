177442e5103bcb3c7c339100e5bc2b4e104178c2
Esther Leonard <53614527+thewarpdrive84@users.noreply.github.com>
2020-04-30 17:20:36 +0100

    background image

--- DogGrooming/App_Start/BundleConfig.cs
+++ DogGrooming/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace DogGrooming
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
